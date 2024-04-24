4f9b659b10ce6cfd09f372443ec41c79488786c7
Zach2027 <144712865+Zach2027@users.noreply.github.com>
2024-04-24 17:00:30 +0000

    save

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+for(var i=1; i<=10; i++){
+    console.log(i*i);
+}
+
+
+
+for(var i=5; i>0; i--){
+    console.log(i);
+    if( i==1){
+        console.log("blastoff")  
+    }
+}
+for(var i=0; i<=50; i+=2){
+    console.log(i)
+}
+
+
+
+
