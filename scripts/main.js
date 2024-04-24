a621ed3e6b4c3abc5cd2dbaf8376302eacdfa836
Zach2027 <144712865+Zach2027@users.noreply.github.com>
2024-04-24 21:44:36 +0000

    done

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,38 @@
-for(var i=1; i<=10; i++){
-    console.log(i*i);
+console.log("Task 1")
+for (var i = 1; i <= 10; i++) {
+    console.log(i * i);
 }
 
 
-
-for(var i=5; i>0; i--){
+console.log("Task 2")
+for (var i = 5; i > 0; i--) {
     console.log(i);
-    if( i==1){
-        console.log("blastoff")  
+    if (i == 1) {
+        console.log("blastoff")
     }
 }
-for(var i=0; i<=50; i+=2){
+console.log("Task 3")
+for (var i = 0; i <= 50; i += 2) {
     console.log(i)
 }
+console.log("Task 4")
+let sum = 0;
+for (var i = 1; i <= 100; i += 1)
+sum += i;
+ {
+    console.log(sum)
+}
+
+
+console.log("Task 5")
+
+let fact = 1;
+for (var i = 1; i <=10; i+= 1)
+fact = fact*i
+{
+    console.log(fact)
+}
+
 
 
 
