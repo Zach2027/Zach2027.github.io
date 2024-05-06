c5487951b0b2af7ddfcd71c2ac80cdb3132945f4
Zach2027 <144712865+Zach2027@users.noreply.github.com>
2024-05-06 16:15:15 +0000

    12

--- scripts/rainbow.js
+++ scripts/rainbow.js
@@ -6,5 +6,8 @@ let rainbowray = ["red", "orange", "tan", "rebeccapurple", "transparent"];
 let ul = document.createElement("ul")
     
 for (let i = 0; i < rainbowray.length; i++){
+    let li = documnet.createElement("li")
+
+
     
 }
